6cd11886d8ec0c8da22fb226547c83e2e20b285a
unknown <slavo@.(none)>
2011-03-13 21:41:36 +0100

    Dodanie liquibase

--- FakturyCore/src/main/java/Hello.java
+++ FakturyCore/src/main/java/Hello.java
@@ -4,6 +4,8 @@
  * Date: 2011-03-07
  * Time: 22:10:35
  */
+
+import org.postgresql.Driver;
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello World");
